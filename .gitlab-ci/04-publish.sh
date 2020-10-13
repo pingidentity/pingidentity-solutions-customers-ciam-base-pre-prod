da5255fe33e2d4dd2ffc5ab63e074ed4d79b7c23
wesleymccollam <wesleymccollam@pingidentity.com>
2020-10-13 16:35:52 -0400

    Connected GitHub Repo

--- .gitlab-ci/04-publish.sh
+++ .gitlab-ci/04-publish.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
+
 echo "Pushing to GitHub..."
 
 git remote add github ssh://git@github.com/pingidentity/pingidentity-solutions-c360.git >/dev/null || echo 'Remote already exists, skipping creation.'
