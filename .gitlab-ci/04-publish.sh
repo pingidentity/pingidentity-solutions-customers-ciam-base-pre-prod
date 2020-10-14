2cfaedadfaf5f3791b6c1d5e010548d7d6d2e43f
wesleymccollam <wesleymccollam@pingidentity.com>
2020-10-14 13:31:24 -0400

    Added TAG variable to publish step

--- .gitlab-ci/04-publish.sh
+++ .gitlab-ci/04-publish.sh
@@ -3,7 +3,8 @@ set -euo pipefail
 
 echo "Pushing to GitHub..."
 
-git remote add github ssh://git@github.com/pingidentity/pingidentity-solutions-c360.git >/dev/null || echo 'Remote already exists, skipping creation.'
-git push github
+git remote add github ssh://git@github.com/pingidentity/pingidentity-solutions-c360.git >/dev/null \
+  >/dev/null || echo 'Remote already exists, skipping creation...'
+git push github "${CI_COMMIT_TAG}"
 
 exit 0
