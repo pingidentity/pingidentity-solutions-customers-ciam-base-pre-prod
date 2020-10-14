4880a10783e3ca1b3381f5fe8b382498d713533a
wesleymccollam <wesleymccollam@pingidentity.com>
2020-10-14 13:52:59 -0400

    Added GitHub SSH Keys to publish step

--- .gitlab-ci/04-publish.sh
+++ .gitlab-ci/04-publish.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
+echo "Generating SSH Keys..."
+
+if [ ! -d "$HOME"/.ssh ]; then
+  echo "Creating .ssh files..."
+  mkdir "$HOME/.ssh" && echo "$GITHUB_RSA_PRIV" > "$HOME/.ssh/id_rsa" && echo "$GITHUB_RSA_PUB" > "$HOME/.ssh/id_rsa.pub"
+fi
+
 echo "Pushing to GitHub..."
 
 git remote add github ssh://git@github.com/pingidentity/pingidentity-solutions-c360.git >/dev/null \
