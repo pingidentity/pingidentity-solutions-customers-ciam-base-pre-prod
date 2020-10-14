28cdb7e089f540be12fb1f0a4012e19d564bec8b
wesleymccollam <wesleymccollam@pingidentity.com>
2020-10-14 16:21:15 -0400

    Publish step modifications for GitHub push

--- .gitlab-ci/04-publish.sh
+++ .gitlab-ci/04-publish.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-git clone "https://${GITLAB_USER}:${GITLAB_TOKEN}@gitlab.corp.pingidentity.com/solutions/customer360.git"
-cd customer360 || exit 97
-git config user.email "pd-solutions@pingidentity.com"
-git config user.name "pd-solutions"
-
 git remote add gh_location "https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/pingidentity/pingidentity-solutions-c360.git"
 
 if test -n "$CI_COMMIT_TAG"
@@ -13,6 +8,6 @@ then
     git push gh_location "$CI_COMMIT_TAG"
 fi
 
-git push gh_location master
+git push gh_location main
 
 exit 0
