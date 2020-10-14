946973222684d29d4fdf7cecfc690b3ce8c9d566
wesleymccollam <wesleymccollam@pingidentity.com>
2020-10-14 13:22:56 -0400

    Edited GitHub publish step

--- .gitlab-ci/04-publish.sh
+++ .gitlab-ci/04-publish.sh
@@ -4,7 +4,6 @@ set -euo pipefail
 echo "Pushing to GitHub..."
 
 git remote add github ssh://git@github.com/pingidentity/pingidentity-solutions-c360.git >/dev/null || echo 'Remote already exists, skipping creation.'
-git branch -M main
-git push -u github main
+git push github
 
 exit 0
