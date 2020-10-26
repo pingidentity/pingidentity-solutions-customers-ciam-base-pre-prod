f5f39e68af7ca46c6877f434ce926d3aaf50a13c
wesleymccollam <wesleymccollam@pingidentity.com>
2020-10-26 15:53:00 -0400

    Edited count containers script and adjusted max time for curl test

--- .gitlab-ci/tests.d/10-count-containers.sh
+++ .gitlab-ci/tests.d/10-count-containers.sh
@@ -4,30 +4,29 @@
 CONTAINERCOUNT=$(docker ps -q $1 -f status=running | wc -l)
 CONTAINERSEXPECTED=6
 SECONDS=0
-SECONDSMAX=300
+SECONDSMAX=60
+SECONDSLIMIT=$(($SECONDSMAX+25))
 
-while [ $CONTAINERCOUNT -eq $CONTAINERSEXPECTED ] || [ $SECONDS -le $SECONDSMAX ]
+while [ $SECONDS -le $SECONDSLIMIT ]
 do
 function countContainers() {
     CONTAINERCOUNT=$(docker ps -q $1 -f status=running | wc -l)
-    echo "$CONTAINERCOUNT containers found. $CONTAINERSEXPECTED containers expected..."
-    echo "Retrying container check. Sleeping 20 seconds..."
-    TIMEREMAINING=$(($SECONDSMAX-$SECONDS))
-    if [ $TIMEREMAINING -ne $SECONDSMAX ]; then
-        echo "$TIMEREMAINING more seconds allowed for test..."
-    fi
     if [ $CONTAINERCOUNT -eq $CONTAINERSEXPECTED ]; then
-        exit
+        echo "$CONTAINERCOUNT containers found. $CONTAINERSEXPECTED containers expected..."
+        exit 0
+    fi
+        echo "$CONTAINERCOUNT containers found. $CONTAINERSEXPECTED containers expected..."
+        TIMEREMAINING=$(($SECONDSMAX-$SECONDS))
+    if [ $SECONDS -le $SECONDSMAX ]; then
+        echo "$TIMEREMAINING more seconds allowed for test..."
+    else
+        echo "Time limit exceeded. $SECONDSMAX seconds are allowed for test!"
+        exit 1
     fi
 
     sleep 20
 }
 countContainers
 done
-
-   if [ $SECONDS -ge $SECONDSMAX ]; then
-        echo "Time limit exceeded. 300 seconds are allowed for test!"
-        exit 1
-   fi
 
 exit 0
